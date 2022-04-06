a0e305c561119eaea33319c5465249576412d02a
smm9509 <smm9509@rit.edu>
2022-04-06 15:20:12 -0400

    homework #1

--- week13hw1/main.cpp
+++ week13hw1/main.cpp
@@ -1,1 +1,52 @@
-aaaaaaa
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+using std::getline;
+
+int main()
+{
+    const int SIZE = 25;
+    vector<double> vect(0);
+    vect.reserve(SIZE);
+
+    for(int i = 0; i < SIZE; ++i)
+    {
+        double temp;
+        cin >> temp;
+        vect.push_back(temp);
+    }
+
+    double sum = 0;
+    for(int i = 0; i < vect.size(); ++i)
+    {
+        sum += vect.at(i);
+    }
+    double average = sum / vect.size();
+
+    ofstream outF;
+    string outFname;
+    cin.ignore();
+    cout << "Enter filename : ";
+    getline(cin, outFname);
+    outF.open(outFname.c_str());
+    if(outF.fail())
+    {
+        printf("could not open the file"
+        " at \"%s\", sorry.\n", outFname);
+        exit(1);
+    }
+
+    outF << average << ' ' << sum << ' ' << endl;
+
+    return 0;
+}
