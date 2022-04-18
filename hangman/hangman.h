1bb67ba3185876f571a7b27448ee7bc3e02ef59f
smm9509 <smm9509@rit.edu>
2022-04-18 15:44:39 -0400

    starting hangman project

---
+++ hangman/hangman.h
@@ -0,0 +1,10 @@
+//	===============================================================
+//  Filename:		hangman.h
+//	Title:			hangman_extracredit
+//	Course:			CPET.121.01/05-06 - Computational Prob Solving I
+//	Developer:		smm9509@rit.edu
+//	Date:			2022-04-18
+//  Description:    declare functions
+//	===============================================================
+
+#pragma once
---
+++ hangman/main.cpp
@@ -0,0 +1,10 @@
+//	===============================================================
+//  Filename:		main.cpp
+//	Title:			hangman_extracredit
+//	Course:			CPET.121.01/05-06 - Computational Prob Solving I
+//	Developer:		smm9509@rit.edu
+//	Date:			2022-04-18
+//  Description:    define the main function
+//	===============================================================
+
+#include "hangman.h"
