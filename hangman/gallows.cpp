fa2044b1a3b94c2afed3dd20c6f072d41fce260b
smm9509 <smm9509@rit.edu>
2022-04-18 15:54:41 -0400

    add a function

---
+++ hangman/gallows.cpp
@@ -0,0 +1,13 @@
+//	===============================================================
+//  Filename:		gallows.cpp
+//	Title:			hangman_extracredit
+//	Course:			CPET.121.01/05-06 - Computational Prob Solving I
+//	Developer:		smm9509@rit.edu
+//	Date:			2022-04-18
+//  Description:    provide a function for printing the gallows in
+//                  the console, with the hangman partially 
+//                  displayed
+//	===============================================================
+
+#include "hangman.h"
+
