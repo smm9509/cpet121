66b884b2e7ad235dbd001021d9a45f1eff341840
smm9509 <smm9509@rit.edu>
2022-04-20 14:53:19 -0400

    create function for printing

---
+++ hangman/print.cpp
@@ -0,0 +1,19 @@
+//	===============================================================
+//  Filename:		print.cpp
+//	Title:			hangman_extracredit
+//	Course:			CPET.121.01/05-06 - Computational Prob Solving I
+//	Developer:		smm9509@rit.edu
+//	Date:			2022-04-20
+//  Description:    allow printing simpler strings to user
+//	===============================================================
+
+#include "hangman.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void print(const string& text)
+{
+    cout << text << endl;
+}
