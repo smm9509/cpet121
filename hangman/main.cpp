62b69761e20cd6d4dc192f16f152d8cc48863bca
smm9509 <smm9509@rit.edu>
2022-04-20 15:42:35 -0400

    header template

--- hangman/main.cpp
+++ hangman/main.cpp
@@ -8,6 +8,25 @@
 //                  define the main function
 //	===============================================================
 
+
+//Template Header Comment for File:
+//	===============================================================
+//  Filename:		
+//  Title:          hangman_extracredit
+//	Course:			CPET.121.01/05-06 - Computational Prob Solving I
+//	Developer:		smm9509@rit.edu
+//	Date:			2022-04-20
+//  Description:	
+//	===============================================================
+
+//Template Header Comment for Function: 
+//	+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//	Name: 		
+//	Input: 		
+//	Output:		
+//	Purpose:	
+//	+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #include "hangman.h"
 
 int main()
