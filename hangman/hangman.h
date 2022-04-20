7dda67648592c8311d0ba4361e8aa6c096772ee1
smm9509 <smm9509@rit.edu>
2022-04-20 15:10:44 -0400

    add project id

--- hangman/hangman.h
+++ hangman/hangman.h
@@ -4,7 +4,8 @@
 //	Course:			CPET.121.01/05-06 - Computational Prob Solving I
 //	Developer:		smm9509@rit.edu
 //	Date:			2022-04-18
-//  Description:    declare functions
+//  Description:    2022S_dp4_Hangman
+//                  declare functions
 //	===============================================================
 
 #pragma once
--- hangman/main.cpp
+++ hangman/main.cpp
@@ -4,7 +4,8 @@
 //	Course:			CPET.121.01/05-06 - Computational Prob Solving I
 //	Developer:		smm9509@rit.edu
 //	Date:			2022-04-18
-//  Description:    define the main function
+//  Description:    2022S_dp4_Hangman
+//                  define the main function
 //	===============================================================
 
 #include "hangman.h"
